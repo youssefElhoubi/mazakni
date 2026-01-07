b826c2fbfd33e3707b8df6e1d6f74876a8c2a12b
youssefElhoubi <elhoubiyoussef@gmail.com>
2026-01-07 11:23:38 +0100

    feat: integrate header component and set up routing for test path

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"/test",
+        loadComponent:()=> import("../app/header/header.component").then(m=>m.HeaderComponent)
+    }
+];
