587e88bc10b79e3a7c7bc4fe92a6d54093838b65
youssefElhoubi <elhoubiyoussef@gmail.com>
2026-01-07 15:18:07 +0100

    made a page for the home and fixed some promlems in it

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,10 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    // {
-    //     path:"",
-    //     loadComponent:()=> import("../app/app.component").then(m=>m.AppComponent)
-    // },
+    {
+        path:"",
+        loadComponent:()=> import("../app/home/home.component").then(m=>m.HomeComponent)
+    },
     {
         path:"a",
         loadComponent:()=> import("../app/music-list/music-list.component").then(m=>m.MusicListComponent)
