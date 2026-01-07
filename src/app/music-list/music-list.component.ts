794d2c0c5e6cb430762899014257fe899aabb1c2
youssefElhoubi <elhoubiyoussef@gmail.com>
2026-01-07 12:04:18 +0100

    feat: created music list component

---
+++ src/app/music-list/music-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-music-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './music-list.component.html',
+  styleUrl: './music-list.component.css'
+})
+export class MusicListComponent {
+
+}
