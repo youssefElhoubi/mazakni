c2823642d81d3fe65b25a6c2c17c6bbc32475a0c
youssefElhoubi <elhoubiyoussef@gmail.com>
2026-01-07 15:00:01 +0100

    feat: enhance music list component with track data and display layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
+    // {
+    //     path:"",
+    //     loadComponent:()=> import("../app/app.component").then(m=>m.AppComponent)
+    // },
     {
-        path:"/test",
-        loadComponent:()=> import("../app/header/header.component").then(m=>m.HeaderComponent)
-    }
+        path:"a",
+        loadComponent:()=> import("../app/music-list/music-list.component").then(m=>m.MusicListComponent)
+    },
 ];
