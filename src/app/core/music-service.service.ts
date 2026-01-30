88c34b56aac1da5e1caa07aacdcdd5e16735fb70
youssefElhoubi <elhoubiyoussef@gmail.com>
2026-01-30 14:45:17 +0100

    fetch the music end point in the home page

--- src/app/core/music-service.service.ts
+++ src/app/core/music-service.service.ts
@@ -1,11 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { MusicType } from '../types/MusicType';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MusicServiceService {
+  private apiUrl: string = "http://localhost:8080/api/music/";
 
   constructor(private http: HttpClient) { }
-  
+
+  getMusicList(): Observable<MusicType[]> {
+    return this.http.get<MusicType[]> (this.apiUrl+"all");
+  }
+
 }
--- src/app/layouts/home/home.component.ts
+++ src/app/layouts/home/home.component.ts
@@ -1,14 +1,29 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { MusicListComponent } from '../music-list/music-list.component';
+import { MusicServiceService } from './../../core/music-service.service';
+import { MusicType } from '../../types/MusicType';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent,MusicListComponent],
+  imports: [HeaderComponent, MusicListComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  music: MusicType[] = [];
+
+  constructor(private musicService: MusicServiceService) { }
+
+  ngOnInit(): void {
+    this.musicService.getMusicList().subscribe({
+      next: (data) => {
+        this.music = data
+        console.log(data);
+      },
+      error: (e) => console.log("there is some error", e)
+    })
+  }
 
 }
