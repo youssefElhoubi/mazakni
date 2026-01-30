15280ce38711802953b5a18c5e8717df8e9f28df
youssefElhoubi <elhoubiyoussef@gmail.com>
2026-01-30 17:12:29 +0100

    refactor: update upload component to use signals and enhance file upload functionality

--- src/app/core/music-service.service.ts
+++ src/app/core/music-service.service.ts
@@ -12,7 +12,19 @@ export class MusicServiceService {
   constructor(private http: HttpClient) { }
 
   getMusicList(): Observable<MusicType[]> {
-    return this.http.get<MusicType[]> (this.apiUrl+"all");
+    return this.http.get<MusicType[]>(this.apiUrl + "all");
+  }
+
+  UploadFile(name: string, description: string, imageFile: File, audioFile: File): Observable<any> {
+    const formData = new FormData();
+
+
+    formData.append('name', name);
+    formData.append('description', description);
+    formData.append('imageFile', imageFile);
+    formData.append('audioFile', audioFile);
+
+    return this.http.post(this.apiUrl, formData);
   }
 
 }
--- src/app/layouts/upload/upload.component.ts
+++ src/app/layouts/upload/upload.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MusicServiceService } from '../../core/music-service.service';
 
 @Component({
   selector: 'app-upload',
@@ -10,25 +11,26 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './upload.component.css'
 })
 export class UploadComponent {
-  
+  constructor(private musicService: MusicServiceService) {}
+
   musicData = {
     name: '',
     description: ''
   };
 
-  selectedImage: File | null = null;
-  selectedAudio: File | null = null;
-  imagePreview: string | ArrayBuffer | null = null;
+  selectedImage= signal<File | null>(null);
+  selectedAudio= signal<File | null>(null);
+  imagePreview= signal<string | ArrayBuffer | null>(null);
 
   // Handle Image Selection
   onImageSelected(event: any) {
     const file = event.target.files[0];
     if (file) {
       this.selectedImage = file;
-      
+
       // Create preview
       const reader = new FileReader();
-      reader.onload = e => this.imagePreview = reader.result;
+      reader.onload = e => this.imagePreview.set(reader.result as string | ArrayBuffer | null);
       reader.readAsDataURL(file);
     }
   }
@@ -42,14 +44,28 @@ export class UploadComponent {
   }
 
   onSubmit() {
-    if (this.musicData.name && this.selectedAudio) {
-      console.log('Submitting:', {
+    console.log(this.selectedAudio);
+    console.log(this.selectedImage);
+
+    
+    if (this.musicData.name && this.selectedAudio()) {
+      const data = {
         ...this.musicData,
-        image: this.selectedImage,
-        audio: this.selectedAudio
+        image: this.selectedImage(),
+        audio: this.selectedAudio()
+      };
+      this.musicService.UploadFile(
+        data.name,
+        data.description,
+        data.image!,
+        data.audio!
+      ).subscribe({
+        next: (response) => {
+          console.log('Upload successful:', response);
+          alert('Music Uploaded Successfully!');
+          // Reset form logic here
+        }
       });
-      alert('Music Uploaded Successfully!');
-      // Reset form logic here
     } else {
     }
   }
