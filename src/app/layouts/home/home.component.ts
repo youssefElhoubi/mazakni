880a8dff9d3cea115b3b158fe176ad9150e7254c
youssefElhoubi <elhoubiyoussef@gmail.com>
2026-01-30 15:18:17 +0100

    made somechanges over the track display

--- src/app/layouts/home/home.component.ts
+++ src/app/layouts/home/home.component.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { MusicListComponent } from '../music-list/music-list.component';
-import { MusicServiceService } from './../../core/music-service.service';
-import { MusicType } from '../../types/MusicType';
+
 
 @Component({
   selector: 'app-home',
@@ -12,18 +11,5 @@ import { MusicType } from '../../types/MusicType';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  music: MusicType[] = [];
-
-  constructor(private musicService: MusicServiceService) { }
-
-  ngOnInit(): void {
-    this.musicService.getMusicList().subscribe({
-      next: (data) => {
-        this.music = data
-        console.log(data);
-      },
-      error: (e) => console.log("there is some error", e)
-    })
-  }
 
 }
--- src/app/layouts/music-list/music-list.component.ts
+++ src/app/layouts/music-list/music-list.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MusicType } from '../../types/MusicType';
+import { MusicServiceService } from '../../core/music-service.service';
 
 @Component({
   selector: 'app-music-list',
@@ -9,51 +11,19 @@ import { CommonModule } from '@angular/common';
   styles: []
 })
 export class MusicListComponent {
-  
-  tracks = [
-    {
-      id: 1,
-      title: "Midnight Echoes",
-      artist: "Neon Horizon",
-      description: "A synth-wave journey through the city lights.",
-      image: "https://cdn-images.dzcdn.net/images/cover/12850901d97d0e4a6de96c7bacebff99/500x500-000000-80-0-0.jpg" // Replace with your image paths
-    },
-    {
-      id: 2,
-      title: "Electric Dreams",
-      artist: "The Voltage",
-      description: "High energy rock anthem for the rebellion.",
-      image: "https://cdn-images.dzcdn.net/images/cover/12850901d97d0e4a6de96c7bacebff99/500x500-000000-80-0-0.jpg"
-    },
-    {
-      id: 3,
-      title: "Silent Storm",
-      artist: "Echo Vibe",
-      description: "Melodic bass lines meeting soft vocals.",
-      image: "https://cdn-images.dzcdn.net/images/cover/12850901d97d0e4a6de96c7bacebff99/500x500-000000-80-0-0.jpg"
-    },
-    {
-      id: 4,
-      title: "Cyber Heart",
-      artist: "Glitch Protocol",
-      description: "Futuristic beats for the modern soul.",
-      image: "https://cdn-images.dzcdn.net/images/cover/12850901d97d0e4a6de96c7bacebff99/500x500-000000-80-0-0.jpg"
-    },
-    {
-      id: 5,
-      title: "Retro overdrive",
-      artist: "Laser Knight",
-      description: "Fast-paced retro tracks from the 80s.",
-      image: "https://cdn-images.dzcdn.net/images/cover/12850901d97d0e4a6de96c7bacebff99/500x500-000000-80-0-0.jpg"
-    },
-    {
-      id: 6,
-      title: "Deep Blue",
-      artist: "Oceanic",
-      description: "Calm and soothing instrumental vibes.",
-      image: "https://cdn-images.dzcdn.net/images/cover/12850901d97d0e4a6de96c7bacebff99/500x500-000000-80-0-0.jpg"
-    }
-  ];
+  music = signal<MusicType[]>([]);
+
+  constructor(private musicService: MusicServiceService) { }
+
+  ngOnInit(): void {
+    this.musicService.getMusicList().subscribe({
+      next: (data) => {
+        this.music.set(data);
+        console.log(data);
+      },
+      error: (e) => console.log("there is some error", e)
+    })
+  }
 
   playTrack(track: any) {
     console.log('Playing:', track.title);
